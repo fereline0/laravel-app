95053d796196f4a60e45eb2ba7901bc432d43f44
fereline0 <shimko.alex.fereline@gmail.com>
2024-12-27 11:03:05 +0300

    release

--- resources/views/users/edit/general.blade.php
+++ resources/views/users/edit/general.blade.php
@@ -6,7 +6,7 @@
             </h2>
 
             <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-                {{ __('Обновите информацию о пользователе и адрес электронной почты вашего аккаунта.') }}
+                {{ __('Обновите общюю информацию о пользователе.') }}
             </p>
         </header>
 
@@ -34,7 +34,7 @@
                 @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && !$user->hasVerifiedEmail())
                     <div>
                         <p class="text-sm mt-2 text-black dark:text-white">
-                            {{ __('Ваш адрес электронной почты не подтвержден.') }}
+                            {{ __('Адрес электронной почты не подтвержден.') }}
 
                             <button form="send-verification"
                                 class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">
@@ -44,7 +44,7 @@
 
                         @if (session('status') === 'verification-link-sent')
                             <p class="mt-2 font-medium text-sm text-green-600 dark:text-green-400">
-                                {{ __('Новое письмо для подтверждения было отправлено на ваш адрес электронной почты.') }}
+                                {{ __('Новое письмо для подтверждения было отправлено на адрес электронной почты.') }}
                             </p>
                         @endif
                     </div>
